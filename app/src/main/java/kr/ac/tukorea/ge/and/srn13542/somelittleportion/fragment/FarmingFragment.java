88917106dfd3474c0142caedfd05c06641572b55
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-05-12 14:30:13 +0900

    custom dialog 추가

--- app/src/main/java/kr/ac/tukorea/ge/and/srn13542/somelittleportion/fragment/FarmingFragment.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/srn13542/somelittleportion/fragment/FarmingFragment.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import kr.ac.tukorea.ge.and.srn13542.somelittleportion.databinding.FragmentFarmingBinding;
 
 
 public class FarmingFragment extends Fragment {
     FragmentFarmingBinding binding;
+
 
     public FarmingFragment() {
         // Fragment 에 빈 생성자 가 없으면 오류 발생?
@@ -26,6 +32,15 @@ public class FarmingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentFarmingBinding.inflate(inflater);
+
+        //ToolMenuButton 클릭 시 dialog 출력 되도록 하기 위한 코드
+        binding.ToolMenuButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return binding.getRoot();
     }
 
