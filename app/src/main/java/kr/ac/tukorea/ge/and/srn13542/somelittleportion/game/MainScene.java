33a30002cb64fa5744848a449a92ed2bdb3ea723
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-05-13 11:00:41 +0900

    메인 씬 추가.

---
+++ app/src/main/java/kr/ac/tukorea/ge/and/srn13542/somelittleportion/game/MainScene.java
@@ -0,0 +1,17 @@
+package kr.ac.tukorea.ge.and.srn13542.somelittleportion.game;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.transition.Scene;
+
+public class MainScene extends Scene {
+    public MainScene(@NonNull ViewGroup sceneRoot) {
+        super(sceneRoot);
+    }
+
+    public MainScene(@NonNull ViewGroup sceneRoot, @NonNull View layout) {
+        super(sceneRoot, layout);
+    }
+}
